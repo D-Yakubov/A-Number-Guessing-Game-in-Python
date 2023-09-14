9a85a2b56efb045bc80a9674bb4e0386b3a012d8
Dastonbek Yakubov <yakubovdastonbek@gmail.com>
2023-09-14 15:42:28 -0400

    code completed

--- main.py
+++ main.py
@@ -21,4 +21,44 @@ def find_num(x=10):
             break
     print(f"Congratulations! You find {guessed_num} with {guesses} guesses!")
     return guesses
-        
+
+def find_num_pc(x=10):
+    input(f"Think a number between 1 and {x}, then press any keyboard. I find it")
+    low = 1
+    high = x
+    guesses =   0
+    while True:
+        guesses += 1
+        if low != high:
+            guessed_num = random.randint(low, high)
+        else:
+            guessed_num = low
+        answer = input(
+            f"You thought {guessed_num} this number?: (if right press 't')"
+            f"I thought number is greater than (+), or less than (-)".lower()
+        )
+        if answer == "-":
+            high = guessed_num - 1
+        elif answer == "+":
+            low = guessed_num + 1
+        else:
+            break
+        print(f"I found it with {guesses} guesses!")
+        return guesses
+
+def play(x=10):
+    again = True
+    while again:
+        guesses_user = find_num(x)
+        guesses_pc = find_num_pc(x)
+
+        if guesses_user > guesses_pc:
+            print(f"I found it with {guesses_pc} guesses and I won!")
+        elif guesses_user < guesses_pc:
+            print(f"You found it with {guesses_pc} guesses and You won! Congratulations!")
+        else:
+            print("Draw!")
+        again = int(input("Do you wanna play again? Yes(1)/No(0):"))
+
+play()
+         
